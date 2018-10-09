46edd6322e89e2d2cf5c92dab3173b9c3418ef1d
Richard Hoang <richi.hoang@gmx.at>
2018-10-09 12:58:38 +0200

    header component added

--- zeitmessung/src/app/app.module.ts
+++ zeitmessung/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { NavigationcardsComponent } from './navigationcards/navigationcards.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationcardsComponent
+    NavigationcardsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
