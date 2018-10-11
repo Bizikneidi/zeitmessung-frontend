72e67822df8d841a073e4ddec10c865ecc71b063
Elias Bürger <elias.buerger@gmx.at>
2018-10-11 10:01:52 +0200

    Added Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationcardsComponent } from './navigationcards/navigationcards.component';
@@ -10,20 +11,23 @@ import { AdminService } from './services/admin/admin.service';
 import { ParticipantService } from './services/participant/participant.service';
 import { ViewerService } from './services/viewer/viewer.service';
 import { HeaderComponent } from './header/header.component';
-
-
+import { AdminComponent } from './admin/admin.component';
+import { ViewerComponent } from './viewer/viewer.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationcardsComponent,
-    HeaderComponent
-   
+    HeaderComponent,
+    AdminComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule.forRoot(environment.appRoutes as Routes)
   ],
   providers: [
     WebsocketService,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { NavigationcardsComponent } from '../app/navigationcards/navigationcards.component';
+import { AdminComponent } from '../app/admin/admin.component';
+import { ViewerComponent } from '../app/viewer/viewer.component';
+
 export const environment = {
-  production: true
+  production: true,
+  appRoutes: [
+    { path: 'participant', component: ViewerComponent },
+    { path: 'viewer', component: ViewerComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: '**', component: NavigationcardsComponent }
+  ]
 };
