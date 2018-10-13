985800af438a9ceb1b523fb01995339e4a4f46bb
Richard Hoang <richi.hoang@gmx.at>
2018-10-13 22:10:34 +0200

    arrow position fixed and logic added

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { faUser, faLock, faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import { AdminService } from '../services/admin/admin.service';
+import { Message, AdminCommands } from '../entities/networking';
+import { TimeMeterState } from '../entities/timemeterstate';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -8,9 +11,37 @@ import { faUser, faLock, faLongArrowAltLeft } from '@fortawesome/free-solid-svg-
 export class AdminComponent implements OnInit {
 
   faArrow = faLongArrowAltLeft;
-  constructor() { }
+  notready = true;
+  message: string;
+  readyMessage = 'A station is connected, press start to start a race!';
+  disabledMessage = 'You can press start, once a station has connected';
+  measureMessage = 'Currently measuring';
+  constructor(public service: AdminService) {
+    this.message = this.disabledMessage;
+  }
 
   ngOnInit() {
+     this.service.state.subscribe((data: TimeMeterState) => {
+        if (data === TimeMeterState.Ready) {
+          this.notready = false;
+          this.message = this.readyMessage;
+
+        }
+        else if (data === TimeMeterState.Measuring) {
+          this.notready = true;
+          this.message = this.measureMessage;
+        }
+        else if (data === TimeMeterState.Disabled) {
+          this.notready = true;
+          this.message = this.disabledMessage;
+        }
+     });
   }
+
+  onStartRunClicked() {
+    this.service.startRun();
+    // this.notready = true;
+  }
+
 
 }
