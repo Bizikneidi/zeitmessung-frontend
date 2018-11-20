b2441081586fda7f46767bd77e9fa4e42ad40fdc
Bizi Kneidi <tobias.kneidinger@gmail.com>
2018-11-20 14:36:15 +0100

    Implemented communication with backend for liveresultlist

--- src/app/components/liveresultlist/liveresultlist.component.ts
+++ src/app/components/liveresultlist/liveresultlist.component.ts
@@ -5,6 +5,7 @@ import { Time } from '../../entities/time';
 import { Race } from '../../entities/race';
 import { ViewerService } from '../../services/viewer/viewer.service';
 import { Subscription } from 'rxjs/Subscription';
+import { FinishedRunner } from '../../entities/finishedrunner';
 
 @Component({
   selector: 'app-liveresultlist',
@@ -14,8 +15,8 @@ import { Subscription } from 'rxjs/Subscription';
 export class LiveresultlistComponent implements OnInit {
 
 
-  participantList: Array<Runner> = [{Starter: 12, Participant: new Participant('Peter', 'Hauer'), Time: new Time, Race: new Race()},
-                                    {Starter: 9, Participant: new Participant('Richard', 'Hoang'), Time: new Time, Race: new Race()}];
+  participantList: Array<Runner> = [{ Starter: 12, Participant: new Participant('Peter', 'Hauer'), Time: new Time, Race: new Race() },
+  { Starter: 9, Participant: new Participant('Richard', 'Hoang'), Time: new Time, Race: new Race() }];
 
 
   // For cleaning up in onDestroy()
@@ -29,11 +30,21 @@ export class LiveresultlistComponent implements OnInit {
 
     // Check for the start of a race
     this.startSubscription = this.viewer.start.subscribe(ms => {
-      this.participantList = ms.Runners
+      this.participantList = ms.Runners;
     });
 
     // Check for the end of a race
-    this.stopSubscription = this.viewer.stop.subscribe(endTime => {
+    this.stopSubscription = this.viewer.stop.subscribe((finishedRunner: FinishedRunner) => {
+      // Set end of time for runner
+      this.participantList.find(item => item.Starter == finishedRunner.Starter).Time.End = finishedRunner.Time
+
+      // Sort list by time
+      this.participantList = this.participantList.sort((a, b) => {
+        if (a.Time.End - a.Time.Start <= 0) {
+          return 1;
+        }
+        return (a.Time.End - a.Time.Start) - (b.Time.End - b.Time.Start);
+      });
     });
   }
 
