f41e9be45f11f3d7298821b62404ea73e7d7ad1b
Bizi Kneidi <tobias.kneidinger@gmail.com>
2018-11-20 13:57:55 +0100

    Added entity for the RunnerFinished-event

---
+++ src/app/entities/finishedrunner.ts
@@ -0,0 +1,6 @@
+// This entity contains data that is sent when a runner finishes the race.
+// Used for adding the runner to the result-list
+export class FinishedRunner {
+    public Starter: number;
+    public Time: number;
+}
