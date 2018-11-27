a247f1991bfb2578f4ee2c48dc9ec95503513508
Bizi Kneidi <tobias.kneidinger@gmail.com>
2018-11-27 14:47:31 +0100

    Changed Data-Types

---
+++ src/app/entities/assignment.ts
@@ -0,0 +1,4 @@
+export class Assignment {
+    public Starter: number;
+    public Time: number;
+}
--- src/app/entities/networking.ts
+++ src/app/entities/networking.ts
@@ -1,43 +1,47 @@
-export class Message<Commands> { // Used to send Commands and Data between websockets
-    public Command: Commands; // Command used to identify purpose of Message
-    public Data: any; // The "Arguments" which come with the command
-}
-
 export enum StationCommands {
-    // SERVER -> STATION
-    StartMeasuring = 0, // Station should start measuring the time
+    //SERVER -> STATION
+    StartMeasuring = 0, //Station should start measuring times
+    StopMeasuring = 1, //Station should stop measuring times
 
-    // STATION -> SERVER
-    MeasuredStart = 1, // Message contains the start time
-    MeasuredStop = 2 // Message contains a stop time
+    //STATION -> SERVER
+    MeasuredStart = 2, //Message contains the start time
+    MeasuredStop = 3 //Message contains a stop time
 }
 
 export enum AdminCommands {
-    // SERVER -> ADMIN
-    Status = 0, // Message contains the current time measurement status
-    RunStart = 1, // Message contains the time and all runners
-    MeasuredStop = 2, // Message contains a stop time
+    //SERVER -> ADMIN
+    Status = 0, //Message contains the current time measurement status
+    RunStart = 1, //Message contains the time and all runners
+    MeasuredStop = 2, //Message contains a stop time
+    RunEnd = 3, //The run has ended (data is null)
 
-    // ADMIN -> SERVER
-    Start = 4, // Admin has pressed the start button and server should start a race
-    AssignTime = 5 // Admin assigned a time to a runner
+    //ADMIN -> SERVER
+    Start = 4, //Admin has pressed the start button and server should start a race
+    AssignTime = 5 //Admin assigned a time to a runner
 }
 
 export enum ViewerCommands {
-    // SERVER -> VIEWER
-    Status = 0, // Message contains the current time measurement status
-    RunStart = 1, // Message contains the time and all runners
-    RunnerFinished = 2, // Message contains a runner who finished the race
-    RunEnd = 4 // The run has ended (data is null)
+    //SERVER -> VIEWER
+    Status = 0, //Message contains the current time measurement status
+    RunStart = 1, //Message contains the time and all runners
+    RunnerFinished = 2, //Message contains a runner who finished the race
+    RunEnd = 4, //The run has ended (data is null),
+    Races = 5, //Message contains all races up to this point
+    Runners = 6, //Message contains all racers for a race
 
-    // VIEWER -> SERVER
-    // nothing here yet...
+    //VIEWER -> SERVER
+    GetRunners = 7 //Viewer is requesting all racers to a race
 }
 
 export enum ParticipantCommands {
-    // SERVER -> PARTICIPANT
-    // nothing here yet...
+    //SERVER -> PARTICIPANT
+    //nothing here yet...
 
-    // PARTICIPANT -> SERVER
-    Register = 0 // Message contains data to register as a participant
+    //PARTICIPANT -> SERVER
+    Register = 0 //Message contains data to register as a participant
 }
+
+export class Message<Commands> { // Used to send Commands and Data between websockets
+    public Command: Commands; // Command used to identify purpose of Message
+    public Data: any; // The "Arguments" which come with the command
+ }
