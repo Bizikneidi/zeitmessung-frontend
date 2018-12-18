403265ba19e133505f397de7b2794be9e5bb88a7
Peter Hauer <pethaudi@yahoo.de>
2018-12-18 13:20:14 +0100

    applied base desing

--- src/app/components/participantracechooser/participantracechooser.component.ts
+++ src/app/components/participantracechooser/participantracechooser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-participantracechooser',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./participantracechooser.component.css']
 })
 export class ParticipantracechooserComponent implements OnInit {
+
+  faArrow = faLongArrowAltLeft;
 
   constructor() { }
 
