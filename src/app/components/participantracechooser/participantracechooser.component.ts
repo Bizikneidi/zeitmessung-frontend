2384538f6a653a6f055beed959befde27ad6f09b
Peter Hauer <pethaudi@yahoo.de>
2018-12-18 13:05:06 +0100

    added components participantracechooser

---
+++ src/app/components/participantracechooser/participantracechooser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-participantracechooser',
+  templateUrl: './participantracechooser.component.html',
+  styleUrls: ['./participantracechooser.component.css']
+})
+export class ParticipantracechooserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
