515682d2a494e7bd62d4fe19ad56ad48f95ee20a
Peter Hauer <pethaudi@yahoo.de>
2018-12-18 13:56:36 +0100

    designed race item

--- src/app/components/participantracechooser/participantracechooser.component.ts
+++ src/app/components/participantracechooser/participantracechooser.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import { faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-participantracechooser',
@@ -9,46 +9,55 @@ import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 export class ParticipantracechooserComponent implements OnInit {
 
   faArrow = faLongArrowAltLeft;
+  faArrowRight = faLongArrowAltRight;
   sampleData = [
     {
       Id: 1,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
     {
       Id: 2,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
     {
       Id: 3,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
     {
       Id: 4,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
     {
       Id: 5,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
     {
       Id: 6,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
     {
       Id: 7,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
     {
       Id: 8,
       Title: "Marathon",
-      Date: 123456789
+      Date: 123456789,
+      Link: ""
     },
   ];
 
