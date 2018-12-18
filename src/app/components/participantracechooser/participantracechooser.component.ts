ed25f122d1e698c64857199bcfeb5758d69f379d
Peter Hauer <pethaudi@yahoo.de>
2018-12-18 13:35:06 +0100

    added sample data

--- src/app/components/participantracechooser/participantracechooser.component.ts
+++ src/app/components/participantracechooser/participantracechooser.component.ts
@@ -9,6 +9,52 @@ import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 export class ParticipantracechooserComponent implements OnInit {
 
   faArrow = faLongArrowAltLeft;
+  sampleData = [
+    {
+      Id: 1,
+      Title: "Marathon",
+      Date: 123456789
+    },
+    {
+      Id: 2,
+      Title: "Marathon",
+      Date: 123456789
+    },
+    {
+      Id: 3,
+      Title: "Marathon",
+      Date: 123456789
+    },
+    {
+      Id: 4,
+      Title: "Marathon",
+      Date: 123456789
+    },
+    {
+      Id: 5,
+      Title: "Marathon",
+      Date: 123456789
+    },
+    {
+      Id: 6,
+      Title: "Marathon",
+      Date: 123456789
+    },
+    {
+      Id: 7,
+      Title: "Marathon",
+      Date: 123456789
+    },
+    {
+      Id: 8,
+      Title: "Marathon",
+      Date: 123456789
+    },
+  ];
+
+  getData() {
+    return this.sampleData;
+  }
 
   constructor() { }
 
