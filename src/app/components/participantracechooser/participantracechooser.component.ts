741d96a45cd26a28c4e6dd3c55e1cecfbdb959a0
Peter Hauer <pethaudi@yahoo.de>
2019-01-08 13:02:38 +0100

    removed other example data

--- src/app/components/participantracechooser/participantracechooser.component.ts
+++ src/app/components/participantracechooser/participantracechooser.component.ts
@@ -11,59 +11,9 @@ export class ParticipantracechooserComponent implements OnInit {
 
   faArrow = faLongArrowAltLeft;
   faArrowRight = faLongArrowAltRight;
-  sampleData = [
-    {
-      Id: 1,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-    {
-      Id: 2,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-    {
-      Id: 3,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-    {
-      Id: 4,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-    {
-      Id: 5,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-    {
-      Id: 6,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-    {
-      Id: 7,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-    {
-      Id: 8,
-      Title: "Marathon",
-      Date: 123456789,
-      Link: ""
-    },
-  ];
 
   getData() {
-    return this.sampleData;
+    return null;
   }
 
   constructor(private router: Router) {
