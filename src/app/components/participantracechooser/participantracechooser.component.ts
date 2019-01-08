78e2a94d07db52f2a346e3ff5ae2213700be7e17
Peter Hauer <pethaudi@yahoo.de>
2019-01-08 13:51:54 +0100

    added fetching of the races and displaying them

--- src/app/components/participantracechooser/participantracechooser.component.ts
+++ src/app/components/participantracechooser/participantracechooser.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { faLongArrowAltLeft, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { ParticipantService } from '../../services/participant/participant.service';
+import { Race } from '../../entities/race';
 
 @Component({
   selector: 'app-participantracechooser',
@@ -12,14 +13,13 @@ export class ParticipantracechooserComponent implements OnInit {
 
   faArrow = faLongArrowAltLeft;
   faArrowRight = faLongArrowAltRight;
-
-  getData() {
-    return null;
-  }
+  races: Array<Race> = [];
 
   constructor(private router: Router, private service: ParticipantService) {
-    //router.#
     service.connect();
+    service.races.subscribe(data => {
+      this.races = data;
+    })
   }
 
   ngOnInit() {
